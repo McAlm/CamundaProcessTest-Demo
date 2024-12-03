cb788015f0f822a78fee413f4862793e8339258a
Stefan Wiese <stefan.wiese@camunda.com>
2024-12-03 11:10:08 +0100

    fixed version

--- src/test/java/com/mcalm/demo/DemoApplicationTests.java
+++ src/test/java/com/mcalm/demo/DemoApplicationTests.java
@@ -3,10 +3,13 @@ package com.mcalm.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
+	/**
+	 * requires a connection to Zeebe
+	 */
 	void contextLoads() {
 	}
 
